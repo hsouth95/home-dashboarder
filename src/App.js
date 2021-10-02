5c0cc0872d9c3d780d4044ea176cfd87e02540b2
Harrison South <hsouth95@hotmail.co.uk>
2021-10-02 15:18:16 +0100

    Add an edit page and the ability to traverse different pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
-
 import Dashboards from "./components/Dashboards";
+import EditPage from "./edit/Edit";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Dashboards />
+      <Switch>
+        <Route exact path="/">
+          <Dashboards />
+        </Route>
+        <Route path="/edit">
+          <EditPage />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/edit/Edit.js
@@ -0,0 +1,10 @@
+
+function EditPage() {
+  return (
+    <div className="App">
+      <h1>Edit Page</h1>
+    </div>
+  );
+}
+
+export default EditPage;
