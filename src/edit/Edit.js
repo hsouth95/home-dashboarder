33372a13f0b49df6eef14ae58ba52e88365049a3
Harrison South <hsouth95@hotmail.co.uk>
2021-10-02 16:45:22 +0100

    Initial test of gridstack - trying react-grid-layout now

--- src/edit/Edit.js
+++ src/edit/Edit.js
@@ -1,10 +1,39 @@
+import React, { useEffect } from "react";
 
-function EditPage() {
-  return (
-    <div className="App">
-      <h1>Edit Page</h1>
-    </div>
-  );
+import { GridStack } from 'gridstack';
+import 'gridstack/dist/gridstack.min.css';
+import 'gridstack/dist/gridstack-h5.js';
+import './edit.css';
+
+class EditPage extends React.Component {
+
+    componentDidMount() {
+        this.grid = GridStack.init({
+            minRow: 1,
+            cellHeight: "70px"
+        });
+
+        this.grid.addWidget({
+            x: 0,
+            y: 0,
+            h: 1,
+            w: 2,
+            id: 0
+        });
+
+        this.grid.addWidget({
+            x: 1, y: 0, id: 1
+        });
+    }
+
+    render() {
+        return (
+            <div className="Edit">
+                <div class="grid-stack">
+                </div>
+            </div>
+        );
+    }
 }
 
 export default EditPage;
